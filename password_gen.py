ddb9901f08a3df85d7b51270d67b62e0da325a01
luis meinert <luis.meinert@gabl.lan>
2021-11-29 11:14:55 -0300

    Efficient code with imports, no need to input variables manually

--- password_gen.py
+++ password_gen.py
@@ -1,25 +1,11 @@
-import random
+import secrets, string
 
-def check_all(str):
-    result_1 = False
-    result_2 = False
-    result_3 = False
-    result_4 = False
+everything = string.ascii_letters + string.digits
+length = int(input("Required size of password: "))
+password = ''.join(secrets.choice(everything)for i in range(length))
+print(password)
 
-    for i in str:
-        if i in lower_case:
-            result_1 = True
-        if i in upper_case:
-            result_2 = True
-        if i in numbers:
-            result_3 = True
-        if i in symbols:
-            result_4 = True
-
-    return result_1 and result_2 and result_3 and result_4
-
-
-
+"""
 lower_case = 'abcdefghijklmnopqrstuvywz'
 upper_case = 'ABCDEFGHIJKLMNOPQRSTUVYWZ'
 numbers = '1234567890'
@@ -29,4 +15,4 @@ everything = lower_case + upper_case + numbers + symbols
 length = 16
 password = ''.join(random.sample(everything,length))
 print(password)
-print(check_all(password))
+print(check_all(password))"""
