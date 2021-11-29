b3831888b649834a013d19e92e518faa4d4f6842
luis meinert <luis.meinert@gabl.lan>
2021-11-29 11:02:11 -0300

    Added check function so there's always at least one of the variables

--- password_gen.py
+++ password_gen.py
@@ -1,11 +1,32 @@
 import random
 
+def check_all(str):
+    result_1 = False
+    result_2 = False
+    result_3 = False
+    result_4 = False
+
+    for i in str:
+        if i in lower_case:
+            result_1 = True
+        if i in upper_case:
+            result_2 = True
+        if i in numbers:
+            result_3 = True
+        if i in symbols:
+            result_4 = True
+
+    return result_1 and result_2 and result_3 and result_4
+
+
+
 lower_case = 'abcdefghijklmnopqrstuvywz'
 upper_case = 'ABCDEFGHIJKLMNOPQRSTUVYWZ'
 numbers = '1234567890'
-symbols = '[]{}()*;/,_-'
+symbols = '[]{}()*;/,-'
 
-tudo = lower_case + upper_case + numbers + symbols
+everything = lower_case + upper_case + numbers + symbols
 length = 16
-password = ''.join(random.sample(tudo,length))
+password = ''.join(random.sample(everything,length))
 print(password)
+print(check_all(password))
