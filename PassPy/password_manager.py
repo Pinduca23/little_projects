e398432b46837252218deea4912acc095a462f45
luis meinert <luis.meinert@gabl.lan>
2021-12-06 10:42:21 -0300

    Added XKCD style passwords

--- PassPy/password_manager.py
+++ PassPy/password_manager.py
@@ -1,28 +1,35 @@
 import secrets
 import string
+import json
 
 
 pass_length = 0
-while True: 
+while True:
     try:
-        pass_length = int(input('Required size of password: '))       
+        pass_length = int(input('Required size of password: '))
         if pass_length <= 3:
-            print('Password must have at least 4 characters')
+            print('Password must have at least 4 characters or words')
         else:
             break
     except:
         print('Not a valid number')
 
 
+def xkcd_password():
+    with open(file='D:\PyProjectLuis\little_projects\wpa2-wordlists\PlainText\words.txt') as o:
+        words = [word.strip() for word in o]
+        password = ' '.join(secrets.choice(words)
+                            for i in range(pass_length)).title()
+        print(password)
+
+
 def alphanumeric_pass():
     while True:
         everything = string.ascii_letters + string.punctuation + string.digits
-        password = ''.join(secrets.choice(everything)for i in range(pass_length))
+        password = ''.join(secrets.choice(everything)
+                           for i in range(pass_length))
         if (any(c.islower() for c in password)
-            and  (c.isupper() for c in password)
-            and sum(c.isdigit() for c in password ) >=1):
+            and (c.isupper() for c in password)
+                and sum(c.isdigit() for c in password) >= 1):
             break
     print(password)
-
-
-alphanumeric_pass()
